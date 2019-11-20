226508606b62f465312ada2d71be07cd4889e689
dtoropov <dtoropov@htc-cs.ru>
2019-11-20 19:16:40 +0400

    empty

---
+++ src/main/java/ru/htccs/den/springboottest/SecurityConfig.java
@@ -0,0 +1,21 @@
+package ru.htccs.den.springboottest;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
+}
